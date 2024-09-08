183e0426a0db9cd157d7445e7137aaaef9136731
Ravindra Yadav <yadavra2005@gmail.com>
2024-09-08 15:19:20 +0530

    init project

--- Fasal_sarathi_Backend/index.js
+++ Fasal_sarathi_Backend/index.js
@@ -1,1 +1,17 @@
-console.log("Fasal-sarathi");
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/post", (req, res) => {
+    console.log("hello");
+    res.send("hello");
+})
+
+
+app.listen(8000, () => {
+    console.log(`server is running on port ${8000}`);
+})
